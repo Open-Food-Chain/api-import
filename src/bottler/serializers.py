54e74071b4823577024d66633b75f1a482c11079
MyloHDEX <mylo@hybriddex.com>
2020-09-06 09:50:37 +0000

    Revert "flake8 formatting"
    This reverts commit 456dde97705a70518ca55a844820a434f0e19878.

--- src/bottler/serializers.py
+++ src/bottler/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import Bottler
 
-
 class BottlerSerializer(serializers.ModelSerializer):
     uuid = serializers.UUIDField(read_only=True)
 
     class Meta:
         model = Bottler
-        fields = ('uuid', 'anfp', 'dfp', 'bnfp', 'pds', 'pde', 'jds', 'jde', 'bbd', 'pc', 'pl', 'rmn', 'pon', 'pop', )
+        fields = ('uuid', 'anfp', 'dfp', 'bnfp', 'pds', 'pde', 'jds', 'jde', 'bbd', 'pc', 'pl', 'rmn', 'pon', 'pop')
+

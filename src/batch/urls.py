1d5cd83cc907cb09a129cc14c9c271912dbe23ff
MyloHDEX <mylo@hybriddex.com>
2020-09-08 10:06:54 +0000

    batch require integrity

--- src/batch/urls.py
+++ src/batch/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('batch', views.BatchView)
-router.register('new', views.BatchNullIntegrity.as_view())
 router.register('integrity', views.IntegrityView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('batch/new/', views.BatchView.as_view({'get': 'require_integrity'}))
 ]
--- src/batch/views.py
+++ src/batch/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Batch, Integrity
 from .serializers import BatchSerializer, IntegritySerializer
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
+# from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
@@ -14,10 +16,13 @@ class BatchView(viewsets.ModelViewSet):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
 
-
-class BatchNullIntegrityView(APIView):
-
-    def get(self, request, format=None):
-        batches = Batch.objects.all()
-        serializer = BatchSerializer(batches, many=True)
+    # from https://stackoverflow.com/a/23836288
+    # from https://www.django-rest-framework.org/api-guide/viewsets/
+    # #marking-extra-actions-for-routing
+    @action(detail=False)  # listview
+    def require_integrity(self, request, pk=None):
+        null_integrity = Batch.objects.filter(
+            integrity_details__isnull=True
+        )
+        serializer = self.get_serializer(null_integrity, many=True)
         return Response(serializer.data)

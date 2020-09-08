0b947a29c6e938105f5e4ae417217feb75a2bfad
MyloHDEX <mylo@hybriddex.com>
2020-09-08 05:48:15 +0000

    sqlite for dev

--- src/batch/urls.py
+++ src/batch/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('batch', views.BatchView)
+router.register('new', views.BatchNullIntegrity.as_view())
 router.register('integrity', views.IntegrityView)
 
 urlpatterns = [
--- src/batch/views.py
+++ src/batch/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Batch, Integrity
 from .serializers import BatchSerializer, IntegritySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class IntegrityView(viewsets.ModelViewSet):
@@ -11,3 +13,11 @@ class IntegrityView(viewsets.ModelViewSet):
 class BatchView(viewsets.ModelViewSet):
     queryset = Batch.objects.all()
     serializer_class = BatchSerializer
+
+
+class BatchNullIntegrityView(APIView):
+
+    def get(self, request, format=None):
+        batches = Batch.objects.all()
+        serializer = BatchSerializer(batches, many=True)
+        return Response(serializer.data)
